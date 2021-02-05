003eb698898f024fd2072ce0e234daa71e3e657a
a-m-dev <a.mirzaei.dev@gmail.com>
2021-02-05 17:04:35 +0330

    home component done

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,7 +1,27 @@
 import "./Home.scss";
 
 const Home = () => {
-  return <article className="container home">THIS IS HOME</article>;
+  return (
+    <article className="container home">
+      <section className="home__desc">
+        <h1>👋 Hello</h1>
+        <h3> I provided three solution for this challenge!</h3>
+        <p>
+          You can chose each of them from navigation bar to check them how they
+          work.
+        </p>
+
+        <ol>
+          <li> First way is a simple modulo kind of O(n) solution.</li>
+          <li>
+            In second solution I am using worker to handle the computation and
+            considering the large amount of input.
+          </li>
+          <li>Third way is the algorithmic way.</li>
+        </ol>
+      </section>
+    </article>
+  );
 };
 
 export default Home;
